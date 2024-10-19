b3ed62cbe63e1a6604f9206182ae6bd5e47943cd
Rainata Putra <kriegcom77@gmail.com>
2024-10-19 20:48:12 +0800

    feat(middleware): add cors

--- api/route/route.go
+++ api/route/route.go
@@ -3,6 +3,7 @@ package route
 import (
 	"github.com/RPW-11/inventory_management_be/api/middleware"
 	"github.com/RPW-11/inventory_management_be/bootstrap"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -10,6 +11,10 @@ import (
 // register all the available routes
 func Setup(env *bootstrap.Env, db *gorm.DB, gin *gin.Engine) {
 	router := gin.Group("v1")
+
+	// CORS setup
+	cors := cors.Default()
+	router.Use(cors)
 
 	// Public routes
 	publicRouter := router.Group("")
